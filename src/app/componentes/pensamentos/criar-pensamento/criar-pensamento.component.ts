6793ce2b652e0aef94bc58baa1db780d5cc6d784
Liz Santos <lizstspsi@outlook.com>
2023-08-29 18:16:13 -0300

    incluindo formularios reativos

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -10,21 +11,25 @@ import { Router } from '@angular/router';
 })
 export class CriarPensamentoComponent implements OnInit {
 
-  pensamento: Pensamento = {
-    conteudo: '',
-    autoria: '',
-    modelo: 'modelo1'
-}
+
+form!: FormGroup
 
   constructor(private service: PensamentoService,
-              private router: Router) { }
+              private router: Router,
+              private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+
+    this.form = this.formBuilder.group({
+      conteudo: ['Digite uma ideia'],
+      objetivo: ['Digite um objetivo'],
+      modelo: ['modelo1']
+    })
   }
 
   criarPensamento() {
     alert('Nova ideia lançada!')
-   this.service.criar(this.pensamento).subscribe(() => {
+   this.service.criar(this.form.value).subscribe(() => {
     this.router.navigate(['/listarPensamentos'])
    })
   }
